3a29558b0eb182b35686597d7a243ec8e11850fb
Matthew Tzou <sonic55099@gmail.com>
2020-12-22 18:05:01 -0500

    updated NewTutorial.py with buttons and methods

--- NewTutorial.py
+++ NewTutorial.py
@@ -2,15 +2,33 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle("Tech With Tim Tutorial!")
+        self.initUI()
+
+    def initUI(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Hello world!")
+        self.label.move(50, 50)
+
+        self.button1 = QtWidgets.QPushButton(self)
+        self.button1.setText("Aqui!")
+        self.button1.clicked.connect(self.clicked)
+
+    def clicked(self):
+        self.label.setText("you pressed the button")
+        self.update()
+
+    def update(self):
+        self.label.adjustSize()
+
+
 def window():
     app = QApplication(sys.argv)
-    window = QMainWindow()
-    window.setGeometry(200, 200, 300, 300)
-    window.setWindowTitle("Tech With Tim Tutorial!")
-
-    label = QtWidgets.QLabel(window)
-    label.setText("Hello world!")
-    label.move(50, 50)
+    window = MyWindow()
 
     window.show()
     sys.exit(app.exec_())
